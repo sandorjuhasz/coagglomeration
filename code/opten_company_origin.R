63e39c510b05b71f7f278417b7fed0f3e77ddda1
sandorjuhasz <juhasz.sandor@krtk.mta.hu>
2023-09-25 11:58:54 +0200

    opten owner table -- WIP

--- code/opten_company_origin.R
+++ code/opten_company_origin.R
@@ -10,4 +10,21 @@ options(dplyr.summarise.inform = FALSE)
 
 # import ownership data
 ownerdata <- fread("../data/opten-dataset/mta_tulaj.csv", encoding='Latin-1')
-table(ownerdata$ceg_orsz)
+
+
+# keep relevant ownership information
+odata <- ownerdata %>%
+  filter(szemely_orsz != "HU" & szemely_orsz != "") %>%
+  group_by(oo_cegj_sz) %>%
+  mutate(total_szem = n_distinct(szemid)) %>%
+  group_by(oo_cegj_sz, total_szem, szemely_orsz) %>%
+  summarise(nr_szem_from_country = n_distinct(szemid)) %>%
+  mutate(country_share = nr_szem_from_country / total_szem) %>%
+  filter(country_share > 0.5) %>%
+  group_by(oo_cegj_sz) %>%
+  filter(country_share == max(country_share)) %>%
+  select(oo_cegj_sz, szemely_orsz) %>%
+  distinct() %>%
+  data.table()
+
+
