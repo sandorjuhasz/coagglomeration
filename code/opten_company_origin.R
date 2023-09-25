ea49d234a8203801cf26950c69aa3a5c77bd9e5c
sandorjuhasz <juhasz.sandor@krtk.mta.hu>
2023-09-25 12:22:27 +0200

    opten owner selection

--- code/opten_company_origin.R
+++ code/opten_company_origin.R
@@ -13,7 +13,7 @@ ownerdata <- fread("../data/opten-dataset/mta_tulaj.csv", encoding='Latin-1')
 
 
 # keep relevant ownership information
-odata <- ownerdata %>%
+odata1 <- ownerdata %>%
   filter(szemely_orsz != "HU" & szemely_orsz != "") %>%
   group_by(oo_cegj_sz) %>%
   mutate(total_szem = n_distinct(szemid)) %>%
@@ -25,6 +25,31 @@ odata <- ownerdata %>%
   filter(country_share == max(country_share)) %>%
   select(oo_cegj_sz, szemely_orsz) %>%
   distinct() %>%
+  rename(owner_country = szemely_orsz) %>%
   data.table()
+odata1$otype <- "szem"
+
+odata2 <- ownerdata %>%
+  filter(ceg_orsz != "HU" & ceg_orsz != "") %>%
+  group_by(oo_cegj_sz) %>%
+  mutate(total_owner_ceg = n_distinct(ceg_nev)) %>%
+  group_by(oo_cegj_sz, total_owner_ceg, ceg_orsz) %>%
+  summarise(nr_ceg_from_country = n_distinct(ceg_nev)) %>%
+  mutate(country_share = nr_ceg_from_country / total_owner_ceg) %>%
+  filter(country_share > 0.5) %>%
+  group_by(oo_cegj_sz) %>%
+  filter(country_share == max(country_share)) %>%
+  select(oo_cegj_sz, ceg_orsz) %>%
+  distinct() %>%
+  rename(owner_country = ceg_orsz) %>%
+  data.table()
+odata2$otype <- "ceg"
 
 
+# select the final owner
+odata <- rbind(odata1, odata2)
+odata <- odata %>%
+  group_by(oo_cegj_sz) %>%
+  mutate(nr_options = n_distinct(owner_country)) %>%
+  data.table()
+  
