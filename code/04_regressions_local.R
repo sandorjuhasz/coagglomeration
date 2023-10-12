02103a60902904439ffece2e533f1d908e7a73ae
sandorjuhasz <juhasz.sandor@krtk.mta.hu>
2023-10-12 14:50:46 +0200

    new IV models -- new data

--- code/04_regressions_local.R
+++ code/04_regressions_local.R
@@ -21,13 +21,18 @@ manuf_and_services <- FALSE
 drop_agric <- FALSE
 drop_public <- FALSE
 us_supply_iv <- FALSE
-wiot_avg_iv <- TRUE
+wiot_avg_iv <- FALSE
 wiot_iv <- FALSE
+ecu_iv <- TRUE
 c_codes <- c("USA", "SWE", "CZE")
-version <- "wiot_"
+version <- "oc_oct_n2_"
+
+
+### 1 ----- create the dataframe for regressions
 
 
 # file from OC
+#mdf3 <- fread(paste0("../data/oc12_2023_oct_2/03_data_output_emp_porter_", region, "_2015_", year, "_based.csv"), sep = ";")
 mdf3 <- fread(paste0("../data/oc11_2023_oct/oc_mdf3_", region, "_", year, "_based.csv"), sep = ";")
 #mdf3 <- fread(paste0("../data/oc11_2023_oct/oc_mdf3_without_Budapest_nuts4_2017_based.csv"), sep = ";")
 #mdf3 <- fread(paste0("../data/oc11_2023_oct/oc_mdf3_single_plants_nuts4_2017_based.csv"), sep = ";")
@@ -85,7 +90,7 @@ mdf3 <- merge(
   all.x = TRUE,
   all.y = FALSE
 )
-mdf3$io_standard <- scale(mdf3$io_norm_wiot)
+mdf3$io_standard_wiot <- scale(mdf3$io_norm_wiot)
 
 
 
@@ -140,7 +145,7 @@ if(wiot_avg_iv == TRUE){
 
 # add US supply IV
 if(us_supply_iv == TRUE){
-  iv_us <- fread("../outputs/us_supply_3digit_nace_nace.csv")
+  iv_us <- fread(paste0("../outputs/us_supply_3digit_nace_nace_", year, ".csv"))
   
   mdf3 <- merge(
     mdf3,
@@ -151,6 +156,24 @@ if(us_supply_iv == TRUE){
   )
   mdf3$iv_io_standard <- scale(mdf3$iv_io_norm)
 }
+
+
+
+# add ECU data as IV
+if(ecu_iv == TRUE){
+  #iv_ecu <- fread(paste0("../outputs/ecu_vat_iv.csv"))
+  iv_ecu <- fread(paste0("../outputs/ecu_vat_iv_no_limit.csv"))
+  
+  mdf3 <- merge(
+    mdf3,
+    dplyr::select(iv_ecu, ind1, ind2, iv_io_norm),
+    by = c("ind1", "ind2"),
+    all.x = TRUE,
+    all.y = FALSE
+  )
+  mdf3$iv_io_standard <- scale(mdf3$iv_io_norm)
+}
+
 
 
 # industry filters
@@ -189,19 +212,23 @@ if(drop_agric == TRUE){
 mdf3 <- subset(mdf3, is.na(coagg_porter_mne)==0 & is.na(coagg_porter_local)==0)
 
 
+summary(ecu00 <- lm(io_standard_vat ~ iv_io_standard, data = mdf3))
+
+
+### 2 ----- baseline models
 
 # baseline models -- EGK
-summary(egk_m01 <- lm(egk_coagg ~ io_standard, data = mdf3))
+summary(egk_m01 <- lm(egk_coagg ~ io_standard_wiot, data = mdf3))
 summary(egk_m02 <- lm(egk_coagg ~ lab_standard, data = mdf3))
-summary(egk_m03 <- lm(egk_coagg ~ io_standard + lab_standard, data = mdf3))
+summary(egk_m03 <- lm(egk_coagg ~ io_standard_wiot + lab_standard, data = mdf3))
 #summary(egk_m03 <- lm(egk_coagg ~ io_standard_vat + lab_standard, data = mdf3))
 #summary(egk_m04 <- lm(egk_coagg ~ io_standard + lab_standard + log_nr_firms1 + log_nr_firms2, data = mdf3))
 
 # baseline models -- Porter
-summary(porter_m01 <- lm(coagg_porter ~ io_standard, data = mdf3))
+summary(porter_m01 <- lm(coagg_porter ~ io_standard_wiot, data = mdf3))
 summary(porter_m02 <- lm(coagg_porter ~ lab_standard, data = mdf3))
-summary(porter_m03 <- lm(coagg_porter ~ io_standard + lab_standard, data = mdf3))
-
+summary(porter_m03 <- lm(coagg_porter ~ io_standard_wiot + lab_standard, data = mdf3))
+#summary(porter_m03 <- lm(egk_coagg ~ io_standard_vat + lab_standard, data = mdf3))
 
 stargazer(egk_m01,
           egk_m02,
@@ -215,9 +242,15 @@ stargazer(egk_m01,
           out = paste0("../outputs/regression_tables/", version, "local_egk_porter_", year, "_", region, ".html"))
 
 
+
+
+
+
+### 2 ----- IV models
+
 # set up the IV part here
-summary(iv_egk <- ivreg::ivreg(egk_coagg ~ io_standard + lab_standard | USA + swe_lab_standard, data = mdf3))
-summary(iv_porter <- ivreg::ivreg(coagg_porter ~ io_standard + lab_standard | USA + swe_lab_standard, data = mdf3))
+summary(iv_egk <- ivreg::ivreg(egk_coagg ~ io_standard_wiot + lab_standard | USA + swe_lab_standard, data = mdf3))
+summary(iv_porter <- ivreg::ivreg(coagg_porter ~ io_standard_wiot + lab_standard | USA + swe_lab_standard, data = mdf3))
 
 c_egk <- coeftest(iv_egk, vcov = vcovCL, cluster = ~ind_pair_id)
 c_porter <- coeftest(iv_porter, vcov = vcovCL, cluster = ~ind_pair_id)
@@ -227,8 +260,10 @@ stargazer(c_egk, c_porter,
 
 
 iv_mdf3 <- subset(mdf3, ind1_2d != ind2_2d)
-summary(iv_egk <- ivreg::ivreg(egk_coagg ~ io_standard + lab_standard | USA + swe_lab_standard, data = iv_mdf3))
-summary(iv_porter <- ivreg::ivreg(coagg_porter ~ io_standard + lab_standard | USA + swe_lab_standard, data = iv_mdf3))
+summary(iv_egk <- ivreg::ivreg(egk_coagg ~ io_standard_wiot + lab_standard | USA + swe_lab_standard, data = iv_mdf3))
+summary(iv_porter <- ivreg::ivreg(coagg_porter ~ io_standard_wiot + lab_standard | USA + swe_lab_standard, data = iv_mdf3))
+#summary(iv_egk <- ivreg::ivreg(egk_coagg ~ io_standard_vat + lab_standard | USA + swe_lab_standard, data = iv_mdf3))
+#summary(iv_porter <- ivreg::ivreg(coagg_porter ~ io_standard_vat + lab_standard | USA + swe_lab_standard, data = iv_mdf3))
 
 
 
@@ -250,13 +285,24 @@ summary(iv_egk <- ivreg::ivreg(egk_coagg ~ io_standard + lab_standard | iv_avg_i
 summary(iv_porter <- ivreg::ivreg(coagg_porter ~ io_standard + lab_standard | iv_avg_io_norm + swe_lab_standard, data = mdf3))
 
 
+# us supply 2017 test
+summary(iv_egk <- ivreg::ivreg(egk_coagg ~ io_standard_vat + lab_standard | iv_io_standard + swe_lab_standard, data = mdf3))
+summary(iv_porter <- ivreg::ivreg(coagg_porter ~ io_standard_vat + lab_standard | iv_io_standard + swe_lab_standard, data = mdf3))
 
 
+# ECU data as IV
+summary(iv_egk <- ivreg::ivreg(egk_coagg ~ io_standard_vat + lab_standard | iv_io_standard + swe_lab_standard, data = mdf3))
+summary(iv_porter <- ivreg::ivreg(coagg_porter ~ io_standard_vat + lab_standard | iv_io_standard + swe_lab_standard, data = mdf3))
+
+
+
+
+### 3 ----- MNE / domestic baseline models
 
 # mne / domestic models -- EGK
-summary(egk_mm01 <- lm(egk_coagg ~ io_standard + lab_standard, data = mdf3))
-summary(egk_mm02 <- lm(egk_coagg_mne ~ io_standard + lab_standard, data = mdf3))
-summary(egk_mm03 <- lm(egk_coagg_local ~ io_standard + lab_standard, data = mdf3))
+summary(egk_mm01 <- lm(egk_coagg ~ io_standard_wiot + lab_standard, data = mdf3))
+summary(egk_mm02 <- lm(egk_coagg_mne ~ io_standard_wiot + lab_standard, data = mdf3))
+summary(egk_mm03 <- lm(egk_coagg_local ~ io_standard_wiot + lab_standard, data = mdf3))
 
 stargazer(egk_mm01,
           egk_mm02,
@@ -267,10 +313,10 @@ stargazer(egk_mm01,
 
 
 # mne / domestic models -- porter
-summary(porter_mm01 <- lm(coagg_porter ~ io_standard + lab_standard, data = mdf3))
-summary(porter_mm02 <- lm(coagg_porter_mne ~ io_standard + lab_standard, data = mdf3))
-summary(porter_mm03 <- lm(coagg_porter_local ~ io_standard + lab_standard, data = mdf3))
-summary(porter_mm04 <- lm(coagg_porter_mne_local ~ io_standard + lab_standard, data = mdf3))
+summary(porter_mm01 <- lm(coagg_porter ~ io_standard_wiot + lab_standard, data = mdf3))
+summary(porter_mm02 <- lm(coagg_porter_mne ~ io_standard_wiot + lab_standard, data = mdf3))
+summary(porter_mm03 <- lm(coagg_porter_local ~ io_standard_wiot + lab_standard, data = mdf3))
+summary(porter_mm04 <- lm(coagg_porter_mne_local ~ io_standard_wiot + lab_standard, data = mdf3))
 
 stargazer(porter_mm01,
           porter_mm02,
@@ -287,9 +333,20 @@ stargazer(porter_mm01,
 
 
 
+
+
+
+### 4 ----- interaction models
+
 # interactions -- baseline -- EGK and Porter
-summary(inter01 <- lm(egk_coagg ~ io_standard * lab_standard, data = mdf3))
-summary(inter02 <- lm(coagg_porter ~ io_standard * lab_standard, data = mdf3))
+summary(inter01 <- lm(egk_coagg ~ io_standard_wiot * lab_standard, data = mdf3))
+summary(inter02 <- lm(coagg_porter ~ io_standard_wiot * lab_standard, data = mdf3))
+summary(inter01 <- lm(egk_coagg ~ io_standard_vat * lab_standard, data = mdf3))
+summary(inter02 <- lm(coagg_porter ~ io_standard_vat * lab_standard, data = mdf3))
+summary(inter01 <- lm(egk_coagg ~ io_norm * sr_norm, data = mdf3))
+summary(inter02 <- lm(coagg_porter ~ io_norm * sr_norm, data = mdf3))
+
+
 #summary(inter03 <- lm(egk_coagg ~ io01 * labor01, data = mdf3))
 #summary(inter04 <- lm(coagg_porter ~ io01 * labor01, data = mdf3))
 
@@ -313,9 +370,9 @@ stargazer(inter01,
 
 
 # interactions for mne / domestic -- EGK
-summary(egk_inter01 <- lm(egk_coagg ~ io_standard * lab_standard, data = mdf3))
-summary(egk_inter02 <- lm(egk_coagg_mne ~ io_standard * lab_standard, data = mdf3))
-summary(egk_inter03 <- lm(egk_coagg_local ~ io_standard * lab_standard, data = mdf3))
+summary(egk_inter01 <- lm(egk_coagg ~ io_standard_wiot * lab_standard, data = mdf3))
+summary(egk_inter02 <- lm(egk_coagg_mne ~ io_standard_wiot * lab_standard, data = mdf3))
+summary(egk_inter03 <- lm(egk_coagg_local ~ io_standard_wiot * lab_standard, data = mdf3))
 
 stargazer(egk_inter01,
           egk_inter02,
@@ -335,7 +392,7 @@ stargazer(egk_inter04,
 
 
 # interactions for mne / domestic -- porter
-summary(p_inter01 <- lm(coagg_porter ~ io_standard * lab_standard, data = mdf3))
+summary(p_inter01 <- lm(coagg_porter ~ io_standard_wiot * lab_standard, data = mdf3))
 summary(p_inter02 <- lm(coagg_porter_mne ~ io_standard * lab_standard, data = mdf3))
 summary(p_inter03 <- lm(coagg_porter_local ~ io_standard * lab_standard, data = mdf3))
 summary(p_inter04 <- lm(coagg_porter_mne_local ~ io_standard * lab_standard, data = mdf3))
@@ -370,6 +427,7 @@ stargazer(p_inter05,
 
 # for interplots -- EGK
 summary(egk_interplot <- lm(egk_coagg ~ io_norm * sr_norm, data = mdf3))
+#summary(egk_interplot <- lm(egk_coagg ~ io_standard_vat * lab_standard, data = mdf3))
 
 # interplot 1 
 title <- "interplot_SR_transactions"
@@ -401,15 +459,15 @@ dev.off()
 
 
 # for interplots -- porter
-#summary(porter_interplot <- lm(coagg_porter ~ io_norm * sr_norm, data = mdf3))
-summary(porter_interplot <- lm(coagg_porter ~ io_norm_wiot * sr_norm, data = mdf3))
+summary(porter_interplot <- lm(coagg_porter ~ io_norm * sr_norm, data = mdf3))
+#summary(porter_interplot <- lm(coagg_porter ~ io_norm_wiot * sr_norm, data = mdf3))
 
 # interplot 1 
 title <- "interplot_porter_SR_transactions"
 file_name <- paste0("../figures/", title, "_", region, ".png")
 png(file_name, width=600, height=600, units = 'px')
 
-interplot(m = porter_interplot, var1 = "sr_norm", var2 = "io_norm_wiot", size=3) +
+interplot(m = porter_interplot, var1 = "sr_norm", var2 = "io_norm", size=3) +
   xlab("IO transactions") +
   ylab("Estimated coefficient for\nlabor flow") +
   #theme_bw() +
@@ -424,7 +482,7 @@ title <- "interplot_porter_transactions_SR"
 file_name <- paste0("../figures/", title, "_", region, ".png")
 png(file_name, width=600, height=600, units = 'px')
 
-interplot(m = porter_interplot, var1 = "io_norm_wiot", var2 = "sr_norm", size=3) +
+interplot(m = porter_interplot, var1 = "io_norm", var2 = "sr_norm", size=3) +
   xlab("Labor flow") +
   ylab("Estimated coefficient for\nIO transactions") +
   geom_hline(yintercept = 0, linetype = "dashed", size=1.5) +
