fa9503dd4109ad4f9d3b0978b24e7f1baf366ac6
sandorjuhasz <juhasz.sandor@krtk.mta.hu>
2023-10-24 12:21:57 +0200

    paper regressions -- WIP

--- code/05_regressions_paper.R
+++ code/05_regressions_paper.R
@@ -100,7 +100,9 @@ stargazer(ive[[1]],
 ### --- table 3 OLS with interactions
 region_codes <- c("nuts3", "nuts4", "city")
 eim <- list()
+eim01 <- list()
 pim <- list()
+pim01 <- list()
 for(r in 1:length(region_codes)){
   # file from OC
   path <- paste0("../data/oc13_2023_oct_3/04oc_data_", version, region_codes[r], "_", focal_year, ".csv")
@@ -124,6 +126,77 @@ stargazer(
   covariate.labels = c("IO connections", "Labor flow", "IO connections X Labor flow"),
   out = paste0("../outputs/regression_tables/03_interactions.html")
 )
+
+
+
+
+### --- interplot figure
+path <- paste0("../data/oc13_2023_oct_3/04oc_data_", version, region_level, "_", focal_year, ".csv")
+reg_df <- prep_baseline_regression_table(path)
+
+# baseline models -- EGK and Porter
+pipm <- lm(coagg_porter_rca01_stand ~ io_wiot_hun * sr_norm, data = reg_df)
+
+
+# interplot 1 
+title <- "figure021_interplot_labor_IO"
+file_name <- paste0("../figures/", title, "_", region_level, ".png")
+png(file_name, width=600, height=600, units = 'px')
+
+ip1 <- interplot(m = pipm,
+          var1 = "sr_norm",
+          var2 = "io_wiot_hun",
+          size = 3,
+          xmin = -1,
+          xmax = 1,
+          rfill = "#6da3d0") +
+  xlab("IO connections") +
+  ylab("Estimated coefficient for\nlabor flow") +
+  #ylim(0, 0.5) +
+  #theme_bw() +
+  geom_hline(yintercept = 0, linetype = "dashed", size=1.5) +
+  theme_cowplot(12) +
+  theme(axis.text = element_text(size=30), axis.title=element_text(size=40))
+dev.off()
+
+
+# interplot 2
+title <- "figure022_interplot_IO_labor"
+file_name <- paste0("../figures/", title, "_", region_level, ".png")
+png(file_name, width=600, height=600, units = 'px')
+
+ip2 <- interplot(m = pipm,
+          var1 = "io_wiot_hun",
+          var2 = "sr_norm",
+          size = 3,
+          xmin = -1,
+          xmax = 1,
+          rfill = "#6da3d0") +
+  xlab("Labor flow") +
+  ylab("Estimated coefficient for\nIO connections") +
+  geom_hline(yintercept = 0, linetype = "dashed", size=1.5) +
+  theme_cowplot(12) +
+  theme(axis.text = element_text(size=30), axis.title=element_text(size=40))
+dev.off()
+
+
+# combined version
+title <- "fig02_interplots"
+file_name <- paste0("../figures/", title, "_", region_level, ".png")
+png(file_name, width=1400, height=650, units = 'px')
+plot_grid(ip1, ip2, labels = c('A', 'B'), label_size = 40)
+dev.off()
+
+
+
+### --- SI -- interaction of dummies
+region_level <- "nuts4"
+path <- paste0("../data/oc13_2023_oct_3/04oc_data_", version, region_level, "_", focal_year, ".csv")
+reg_df <- prep_baseline_regression_table(path)
+
+summary(i01_egk <- lm(egk_coagg_stand ~ io_wiot01 * labor01, data = reg_df))
+summary(i01_cpe <- lm(coagg_porter_emp_stand ~ io_wiot01 * labor01, data = reg_df))
+summary(i01_cpr <- lm(coagg_porter_rca01_stand ~ io_wiot01 * labor01, data = reg_df))
 
 
 
