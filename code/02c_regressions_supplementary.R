1b43adb501d259f794687d8b4add1f753457907e
sandorjuhasz <juhasz.sandor@krtk.mta.hu>
2024-12-13 11:35:48 +0100

    revision -- wip

--- code/02c_regressions_supplementary.R
+++ code/02c_regressions_supplementary.R
@@ -9,6 +9,7 @@ library(sandwich)
 library(fixest)
 library(interplot)
 library(cowplot)
+library(margins)
 source("../code/02_regression_functions.R")
 
 
@@ -936,6 +937,248 @@ sint_m8 <- feols(coagg_porter_rca01_stand ~ io3_stand * lab_stand | ind1 + ind2,
 
 
 etable(sint_m1, sint_m2, sint_m3, sint_m4, sint_m5, sint_m6, sint_m7, sint_m8)
+
+
+
+
+
+### revision -- citation based relatedness
+reg_df <- combined_regression_df(regions, export = TRUE)
+cr_norm <- fread("../outputs/cr_norm_citation.csv")
+cr_norm$cr_stand <- scale(cr_norm$cr_norm)
+
+reg_df <- merge(
+  reg_df,
+  cr_norm,
+  by.x = c("ind1_2d", "ind2_2d"),
+  by.y = c("ind_i", "ind_j"),
+  all.x = TRUE,
+  all.y = FALSE
+)
+
+
+# main OLS
+m1 <- feols(egk_coagg_stand_nuts3 ~ io_wiot_hun_stand + lab_stand + cr_norm,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m2 <- feols(egk_coagg_stand_nuts4 ~ io_wiot_hun_stand + lab_stand + cr_norm,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m3 <- feols(egk_coagg_stand_nuts3 ~ io3_stand + lab_stand + cr_norm,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m4 <- feols(egk_coagg_stand_nuts4 ~ io3_stand + lab_stand + cr_norm,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+
+m5 <- feols(coagg_porter_rca01_stand_nuts3 ~ io_wiot_hun_stand + lab_stand + cr_norm,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m6 <- feols(coagg_porter_rca01_stand_nuts4 ~ io_wiot_hun_stand + lab_stand + cr_norm,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m7 <- feols(coagg_porter_rca01_stand_nuts3 ~ io3_stand + lab_stand + cr_norm,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m8 <- feols(coagg_porter_rca01_stand_nuts4 ~ io3_stand + lab_stand + cr_norm,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+
+etable(m1, m2, m3, m4, m5, m6, m7, m8)
+
+
+
+# interaction mdoels
+int_m1 <- feols(egk_coagg_stand_nuts4 ~ io_wiot_hun_stand * lab_stand + cr_norm,
+                cluster = ~ind1 + ind2,
+                data = reg_df)
+int_m2 <- feols(egk_coagg_stand_nuts4 ~ io_wiot_hun_stand * lab_stand + cr_norm | ind1 + ind2,
+                cluster = ~ind1 + ind2,
+                data = reg_df)
+int_m3 <- feols(egk_coagg_stand_nuts4 ~ io3_stand * lab_stand + cr_norm,
+                cluster = ~ind1 + ind2,
+                data = reg_df)
+int_m4 <- feols(egk_coagg_stand_nuts4 ~ io3_stand * lab_stand + cr_norm | ind1 + ind2,
+                cluster = ~ind1 + ind2,
+                data = reg_df)
+
+int_m5 <- feols(coagg_porter_rca01_stand_nuts4 ~ io_wiot_hun_stand * lab_stand + cr_norm,
+                cluster = ~ind1 + ind2,
+                data = reg_df)
+int_m6 <- feols(coagg_porter_rca01_stand_nuts4 ~ io_wiot_hun_stand * lab_stand + cr_norm | ind1 + ind2,
+                cluster = ~ind1 + ind2,
+                data = reg_df)
+int_m7 <- feols(coagg_porter_rca01_stand_nuts4 ~ io3_stand * lab_stand + cr_norm,
+                cluster = ~ind1 + ind2,
+                data = reg_df)
+int_m8 <- feols(coagg_porter_rca01_stand_nuts4 ~ io3_stand * lab_stand + cr_norm | ind1 + ind2,
+                cluster = ~ind1 + ind2,
+                data = reg_df)
+
+etable(int_m1, int_m2, int_m3, int_m4, int_m5, int_m6, int_m7, int_m8)
+
+
+
+# interplot data generation -- WIOD
+# fit the linear regression model with clustered standard errors
+int_m1 <- lm(egk_coagg_stand_nuts4 ~ io_wiot_hun * sr_norm + cr_norm, data = reg_df)
+clustered_se <- vcovCL(int_m1, vcov = vcovCL, cluster = ~ind1 + ind2)
+
+# calculate marginal effects using clustered standard errors
+marginal_effects11 <- margins(
+  int_m1,
+  variables = "io_wiot_hun",
+  at = list(sr_norm = seq(-1, 1, 0.1)), 
+  vcov = clustered_se
+)
+mf11 <- data.table(summary(marginal_effects11)) %>%
+  rename(
+    x_values = sr_norm,
+    y_values = factor
+  )
+mf11$dep_var <- "egk_coagg_stand_nuts4"
+mf11$x_var <- "sr_norm"
+
+
+# calculate marginal effects using clustered standard errors
+marginal_effects12 <- margins(
+  int_m1,
+  variables = "sr_norm",
+  at = list(io_wiot_hun = seq(-1, 1, 0.1)), 
+  vcov = clustered_se
+)
+mf12 <- data.table(summary(marginal_effects12)) %>%
+  rename(
+    x_values = io_wiot_hun,
+    y_values = factor
+  )
+mf12$dep_var <- "egk_coagg_stand_nuts4"
+mf12$x_var <- "io_wiot_hun"
+
+
+# fit the linear regression model with clustered standard errors
+int_m2 <- lm(coagg_porter_rca01_stand_nuts4 ~ io_wiot_hun * sr_norm + cr_norm, data = reg_df)
+clustered_se <- vcovCL(int_m2, vcov = vcovCL, cluster = ~ind1 + ind2)
+
+# calculate marginal effects using clustered standard errors
+marginal_effects21 <- margins(
+  int_m2,
+  variables = "io_wiot_hun",
+  at = list(sr_norm = seq(-1, 1, 0.1)), 
+  vcov = clustered_se
+)
+mf21 <- data.table(summary(marginal_effects21)) %>%
+  rename(
+    x_values = sr_norm,
+    y_values = factor
+  )
+mf21$dep_var <- "coagg_porter_rca01_stand_nuts4"
+mf21$x_var <- "sr_norm"
+
+
+# calculate marginal effects using clustered standard errors
+marginal_effects22 <- margins(
+  int_m2,
+  variables = "sr_norm",
+  at = list(io_wiot_hun = seq(-1, 1, 0.1)), 
+  vcov = clustered_se
+)
+mf22 <- data.table(summary(marginal_effects22)) %>%
+  rename(
+    x_values = io_wiot_hun,
+    y_values = factor
+  )
+mf22$dep_var <- "coagg_porter_rca01_stand_nuts4"
+mf22$x_var <- "io_wiot_hun"
+
+
+
+# interplot data generation -- transactions
+# fit the linear regression model with clustered standard errors
+int_m3 <- lm(egk_coagg_stand_nuts4 ~ io_norm3 * sr_norm + cr_norm, data = reg_df)
+clustered_se <- vcovCL(int_m3, vcov = vcovCL, cluster = ~ind1 + ind2)
+
+
+# calculate marginal effects using clustered standard errors
+marginal_effects31 <- margins(
+  int_m3,
+  variables = "io_norm3",
+  at = list(sr_norm = seq(-1, 1, 0.1)), 
+  vcov = clustered_se
+)
+mf31 <- data.table(summary(marginal_effects31)) %>%
+  rename(
+    x_values = sr_norm,
+    y_values = factor
+  )
+mf31$dep_var <- "egk_coagg_stand_nuts4"
+mf31$x_var <- "sr_norm"
+
+
+# calculate marginal effects using clustered standard errors
+marginal_effects32 <- margins(
+  int_m3,
+  variables = "sr_norm",
+  at = list(io_norm3 = seq(-1, 1, 0.1)), 
+  vcov = clustered_se
+)
+mf32 <- data.table(summary(marginal_effects32)) %>%
+  rename(
+    x_values = io_norm3,
+    y_values = factor
+  )
+mf32$dep_var <- "egk_coagg_stand_nuts4"
+mf32$x_var <- "io_norm3"
+
+
+
+# fit the linear regression model with clustered standard errors
+int_m4 <- lm(coagg_porter_rca01_stand_nuts4 ~ io_norm3 * sr_norm + cr_norm, data = reg_df)
+clustered_se <- vcovCL(int_m4, vcov = vcovCL, cluster = ~ind1 + ind2)
+
+# calculate marginal effects using clustered standard errors
+marginal_effects41 <- margins(
+  int_m4,
+  variables = "io_norm3",
+  at = list(sr_norm = seq(-1, 1, 0.1)), 
+  vcov = clustered_se
+)
+mf41 <- data.table(summary(marginal_effects41)) %>%
+  rename(
+    x_values = sr_norm,
+    y_values = factor
+  )
+mf41$dep_var <- "coagg_porter_rca01_stand_nuts4"
+mf41$x_var <- "sr_norm"
+
+
+# calculate marginal effects using clustered standard errors
+marginal_effects42 <- margins(
+  int_m4,
+  variables = "sr_norm",
+  at = list(io_norm3 = seq(-1, 1, 0.1)), 
+  vcov = clustered_se
+)
+mf42 <- data.table(summary(marginal_effects42)) %>%
+  rename(
+    x_values = io_norm3,
+    y_values = factor
+  )
+mf42$dep_var <- "coagg_porter_rca01_stand_nuts4"
+mf42$x_var <- "io_norm3"
+
+
+mf_df <- rbind(mf11, mf12, mf21, mf22, mf31, mf32, mf41, mf42)
+
+
+# export
+write.table(mf_df,
+            paste0("../outputs/marginal_effects_for_interplots_with_citations.csv"),
+            row.names = FALSE,
+            col.names = TRUE,
+            sep = ";"
+)
+
 
 
 
--- code/06_revision.R
+++ code/06_revision.R
@@ -1,9 +1,15 @@
-###### patent data manipulation for revision  -- sandorjuhasz
+###### revision related calculation -- sandorjuhasz
+
+
+
+###### 01 patent data ######
+###### 02 patent data ######
 
 
 
 library(data.table)
 library(dplyr)
+library(fixest)
 source("../code/02_regression_functions.R")
 
 
@@ -11,6 +17,10 @@ source("../code/02_regression_functions.R")
 # parameters
 focal_year <- 2017
 regions <- c("nuts3", "nuts4")
+
+
+
+###### 01 patent data ######
 
 
 
@@ -65,7 +75,7 @@ gc()
 
 # EPO patents in Hungary 2015-2017
 epo_df <- fread("../outputs/epo_2015_2017_Hungary.csv")
-epo_df <- select(epo_df, appln_id, ctry_code) %>% unique()
+epo_df <- dplyr::select(epo_df, appln_id, ctry_code) %>% unique()
 
 # citation table
 cdf <- fread("../data/patent_data/OECD_patent_data_2024SEPT/OECD_CITATIONS_202408/202408_EPO_CITATIONS.txt")
@@ -92,11 +102,11 @@ cdf <- merge(
 # all cited inside Hungary
 ctable <- cdf %>%
   filter((ctry_code_citing == "HU") & (ctry_code_cited == "HU")) %>%
-  select(appln_id_citing = Citing_appln_id, appln_id_cited = Cited_Appln_id)
+  dplyr::select(appln_id_citing = Citing_appln_id, appln_id_cited = Cited_Appln_id)
   
 # add tech codes to citation relations in Hungary
 epo_df <- fread("../outputs/epo_2015_2017_Hungary.csv")
-epo_df <- select(epo_df, appln_id, IPC4d) %>% unique()
+epo_df <- dplyr::select(epo_df, appln_id, IPC4d) %>% unique()
 
 # add tech codes to citation table
 ctable <- merge(
@@ -125,7 +135,7 @@ ctable <- subset(ctable, IPC4d_citing != IPC4d_cited)
 # concordance table
 conc <- fread("../data/patent_data/correspondance_ipc_nace.csv")
 conc$ind <- substr(conc$NACE2, 1, 2)
-conc <- select(conc, ipc = IPCV2015, ind) %>% unique()
+conc <- dplyr::select(conc, ipc = IPCV2015, ind) %>% unique()
 
 # add nace codes to citation table
 cnace <- merge(
@@ -185,7 +195,7 @@ cr_norm <- cnace %>%
   data.table()
 
 cr_norm$cr_norm[is.na(cr_norm$cr_norm) == 1] <- -1
-cr_norm <- subset(cr_norm, ind_i < ind_j)
+#cr_norm <- subset(cr_norm, ind_i < ind_j)
 
 # export
 write.table(cr_norm,
@@ -194,4 +204,88 @@ write.table(cr_norm,
             col.names = TRUE,
             sep = ";"
 )
+
+
+
+
+
+
+###### 02 effect comparison -- IO max and labor average  ######
+
+# data prep
+reg_df <- combined_regression_df(regions, export = TRUE)
+
+
+
+
+### --- Table 2 -- clustered SE
+m1 <- feols(egk_coagg_stand_nuts3 ~ io_wiot_hun_stand + lab_stand,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m2 <- feols(egk_coagg_stand_nuts4 ~ io_wiot_hun_stand + lab_stand,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m3 <- feols(egk_coagg_stand_nuts3 ~ io3_stand + lab_stand,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m4 <- feols(egk_coagg_stand_nuts4 ~ io3_stand + lab_stand,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+
+m5 <- feols(coagg_porter_rca01_stand_nuts3 ~ io_wiot_hun_stand + lab_stand,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m6 <- feols(coagg_porter_rca01_stand_nuts4 ~ io_wiot_hun_stand + lab_stand,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m7 <- feols(coagg_porter_rca01_stand_nuts3 ~ io3_stand + lab_stand,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+m8 <- feols(coagg_porter_rca01_stand_nuts4 ~ io3_stand + lab_stand,
+            cluster = ~ind1 + ind2,
+            data = reg_df)
+
+
+
+
+calculate_effects <- function(model_name, io_var, labor_var, data, model_label) {
+  # extract coefficients
+  coef_IO <- coef(model_name)[io_var]
+  coef_labor <- coef(model_name)[labor_var]
+  
+  # calculate max and mean values for the variables
+  max_IO <- max(data[[io_var]], na.rm = TRUE)
+  mean_labor <- mean(data[[labor_var]], na.rm = TRUE)
+  
+  # calculate the effects
+  max_IO_effect <- coef_IO * max_IO
+  avg_labor_effect <- coef_labor * mean_labor
+  ratio <- max_IO_effect / avg_labor_effect
+  
+  # save results in a data.table
+  results <- data.table(
+    model_name = model_label,
+    effect_name = c("max_IO_effect", "avg_labor_effect", "ratio"),
+    value = c(max_IO_effect, avg_labor_effect, ratio)
+  )
+  
+  return(results)
+}
+
+
+
+effects <- data.table()
+effects <- rbind(effects, calculate_effects(m1, "io_wiot_hun_stand", "lab_stand", reg_df, "m1"))
+effects <- rbind(effects, calculate_effects(m2, "io_wiot_hun_stand", "lab_stand", reg_df, "m2"))
+effects <- rbind(effects, calculate_effects(m3, "io3_stand", "lab_stand", reg_df, "m3"))
+effects <- rbind(effects, calculate_effects(m4, "io3_stand", "lab_stand", reg_df, "m4"))
+effects <- rbind(effects, calculate_effects(m5, "io_wiot_hun_stand", "lab_stand", reg_df, "m5"))
+effects <- rbind(effects, calculate_effects(m6, "io_wiot_hun_stand", "lab_stand", reg_df, "m6"))
+effects <- rbind(effects, calculate_effects(m7, "io3_stand", "lab_stand", reg_df, "m7"))
+effects <- rbind(effects, calculate_effects(m8, "io3_stand", "lab_stand", reg_df, "m8"))
+
+
+
+
+
 
